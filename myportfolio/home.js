918a84882ccab240e49bfc9d3a2a79ffe2618a07
VikasDevel <103916621+VikasDevel@users.noreply.github.com>
2022-11-04 19:52:47 +0530

    Add files via upload

---
+++ myportfolio/home.js
@@ -0,0 +1,27 @@
+// function myFunction() {
+//     var x = document.getElementById("mytopnav2");
+//     if (x.className === "topnav2") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav2";
+//     }
+// }
+
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function () { myFunction() };
+
+// // Get the navbar
+// var topnav = document.getElementById("topnav");
+
+// // Get the offset position of the navbar
+// var sticky = topnav.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         topnav.classList.add("sticky")
+//     } else {
+//         topnav.classList.remove("sticky");
+//     }
+// }
+   console.log("vikas");
